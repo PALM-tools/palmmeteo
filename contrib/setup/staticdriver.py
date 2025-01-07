0ff7c8b78527e0bb385d0c9e73dd445b1222fd07
Pavel Krc <krc@cs.cas.cz>
2025-01-07 09:25:35 +0100

    Avoid terrain in mass balancing
    This fixes the mass-disbalanced state due to mass balancing being
    calculated and applied also below terrain. However the solution is still
    not perfect if there are also buildings on the boundary.
    
    # Conflicts:
    #	core/builtin.py

--- contrib/setup/staticdriver.py
+++ contrib/setup/staticdriver.py
@@ -29,6 +29,7 @@ from core.runtime import rt
 from core.plugins import SetupPluginMixin
 from core.logging import die, warn, log, verbose
 
+na_ = np.newaxis
 
 class StaticDriverPlugin(SetupPluginMixin):
     """Default setup plugin for loading domain info from static driver file."""
@@ -86,7 +87,23 @@ class StaticDriverPlugin(SetupPluginMixin):
             rt.terrain_rel = ncs.variables['zt'][:]
         else:
             rt.terrain_rel = np.zeros([rt.ny,rt.nx])
-        rt.th = np.ceil(rt.terrain_rel / rt.dz)
+
+        # Check terrain
+        terrain_min = rt.terrain_rel.min()
+        if rt.nested_domain and terrain_min != 0:
+            warn('The lowest point of the terrain variable zt in the parent '
+                'domain is {} (relative to origin_z={}). Please check '
+                'that the lowest point of ALL domains equals zero, otherwise PALM '
+                'shifts the terrain to ensure that, which can lead to vertical '
+                'mismatching with the dynamic driver.', rt.origin_z, terrain_min)
+
+        # Calculate terrain height in integral grid points, which is also the
+        # k-coordinate of the lowest air-cell.
+        # NOTE: PALM assigns terrain to those grid cells whose center lies on
+        # or below terrain (assuming that it is not shifted due to the lowest
+        # point not being 0).
+        rt.th = np.floor(rt.terrain_rel / rt.dz + 0.5).astype('i8')
+        rt.terrain_mask = np.arange(rt.nz)[:,na_,na_] < rt.th[na_,:,:]
 
         # building height
         if 'buildings_3d' in ncs.variables.keys():
